cdc567abb8a31fab4028a2001800e371d755816b
Tej Jain <tejkumar.2103@gmail.com>
2024-03-17 12:51:43 +0530

    first commit

---
+++ src/components/Getstarted/Getstarted.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Getstarted.css";
+
+const Getstarted = () => {
+    return (
+        <section className="c-wrapper">
+            <div className="paddings innerWidth g-container">
+                <div className="flexColCenter inner-container">
+                    <span className="primaryText">Get Started with Homyz</span>
+                    <span className="secondaryText">Subscribe and fine super attractive price quotes
+                        <br />
+                        Find your residence soon</span>
+                    <button className="button">
+                        <a href="mailto:tejkumar.2103@gmail.com">Get Started</a>
+                    </button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Getstarted
